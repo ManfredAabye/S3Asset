64c40445ba7788daeff1cbfc3a70cbf1c9b424aa
Manfred Aabye <zainhofer@gmx.de>
2026-04-27 14:02:58 +0200

    Add files via upload

---
+++ OpenSim/Services/S3AssetService/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OpenSim.Services.S3AssetService")]
+[assembly: AssemblyDescription("S3-backed scalable asset service for OpenSimulator")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("http://opensimulator.org")]
+[assembly: AssemblyProduct("OpenSim")]
+[assembly: AssemblyCopyright("OpenSimulator developers")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a3f7c2b1-4e85-4d19-9f6a-2c8e1d3b7f04")]
+
+[assembly: AssemblyVersion(OpenSim.VersionInfo.AssemblyVersionNumber)]
